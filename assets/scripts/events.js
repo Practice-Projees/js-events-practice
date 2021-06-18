16b76ea3d6662d2f7478c2cc58f3261c3415fc3b
bcooperz1995 <brandon.cooper@mrisoftware.com>
2021-06-18 10:29:27 +1000

    Testing form submission with button click

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -10,17 +10,25 @@ div.addEventListener("click", event => {
     console.log(event);
 })
 
-const button = document.querySelector('button');
+const button = document.querySelector('[type=submit]');
 button.addEventListener("click", (event) => {
-    event.stopImmediatePropagation();
-    event.preventDefault();
+    // event.stopImmediatePropagation();
+    // event.preventDefault();
     console.log("clicked button");
     console.log(event);
-});
+}, );
 
 const listItems = document.querySelectorAll('li');
 const list = document.querySelector('ul');
 
+const form = document.querySelector('form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    console.log("form submit");
+})
+
 list.addEventListener('click', event => {
+    console.log("list event");
     event.target.closest('li').classList.toggle('highlight');
+    button.click();
 });
